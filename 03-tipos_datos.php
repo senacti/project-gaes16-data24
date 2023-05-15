ccb8cadefbaf3f60522aed509c30d386644b4ba0
David Bohorquez <davidbcaro@gmail.com>
2023-05-15 13:47:12 -0500

    Se crean archivos PHP

---
+++ 03-tipos_datos.php
@@ -0,0 +1,17 @@
+<?php
+// Tipos de datos en PHP
+// String
+$nombre = "Juan";
+$Nombre = "David";
+$apellido = "Pérez";
+// Integer
+$edad = 30;
+// Float
+$peso = 80.5;
+// Boolean
+$casado = true;
+// Array
+$colores = array("rojo", "verde", "azul");
+
+echo $nombre . " " . $apellido . " tiene " . $edad . " años";
+?>
---
+++ 04-operadores.php
@@ -0,0 +1,22 @@
+<?php
+$numero1 = 3;
+$numero2 = 3;
+// Suma
+$resultado = $numero1 + $numero2;
+echo "El resultado de la suma es: ", $resultado, "\n";
+// Resta
+$resultado = $numero1 - $numero2;
+echo "El resultado de la resta es: ", $resultado, "\n";
+// Multiplicación
+$resultado = $numero1 * $numero2;
+echo "El resultado de la multiplicación es: ", $resultado, "\n";
+// División
+$resultado = $numero1 / $numero2;
+echo "El resultado de la división es: ", $resultado, "\n";
+// Módulo
+$resultado = $numero1 % $numero2;
+echo "El resultado del módulo es: ", $resultado, "\n";
+// Potencia
+$resultado = $numero1 ** $numero2;
+echo "El resultado de la potencia es: ", $resultado, "\n";
+?>
