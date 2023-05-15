31ac3791b243d6e4e2706eae7f3670ea820c0d2c
Wirley-Valencia <127152303+Wirley-Valencia@users.noreply.github.com>
2023-05-15 13:50:00 -0500

    Add files via upload

---
+++ 04.readline.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// operadores en PHP
+
+$numero1 = readline(" Ingrese el primer numero");
+
+$numero2 = readline(" Ingrese el segundo numero");
+
+//suma
+
+$suma = $numero1 + $numero2;
+
+echo "Suma: $suma";
+
+?>
+
+
